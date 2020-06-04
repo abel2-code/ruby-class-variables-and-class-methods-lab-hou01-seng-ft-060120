ad5c4071263a077503868787eac64fa63fe72f42
Abelardo Miranda <available_email_address@yahoo.com>
2020-06-04 16:13:12 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+
+  @@count = 0
+
+  def Song.count
+    @@count
+  end
+
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+  end
+
+end
