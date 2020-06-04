2ac930bcb216bbf9923ce23d6ab443616252f221
Abelardo Miranda <available_email_address@yahoo.com>
2020-06-04 16:38:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,7 @@ class Song
       @@genres.uniq
     end
 
-    def Song.genre_count
+    def self.genre_count
       genre_count = {}
       @@genres.each do |genre|
         if genre_count[genre]
