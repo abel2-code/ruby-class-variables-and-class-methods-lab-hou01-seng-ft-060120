e3e7677951711c405d98faf8c165de61549ceeab
Abelardo Miranda <available_email_address@yahoo.com>
2020-06-04 16:43:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
         if genre_count[genre]
           genre_count += 1
         else
-          genre_count[genre] = 1
+          genre_count = 1
         end
       end
       genre_count
